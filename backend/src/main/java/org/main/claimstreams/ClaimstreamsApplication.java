8d80d44f61c16cfee7f1c33813b8deddc210d374
anishdass <anishdassatoffice@gmail.com>
2026-09-01 09:09:48 +0100

    feat: made some changes after deploying the application in docker

--- backend/src/main/java/org/main/claimstreams/ClaimstreamsApplication.java
+++ backend/src/main/java/org/main/claimstreams/ClaimstreamsApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClaimstreamsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClaimstreamsApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(ClaimstreamsApplication.class, args);
+    }
 
 }
