1881bf64a6eb9f41046cea932eb32e6b565a8d7a
anishdass <anishdassatoffice@gmail.com>
2026-09-08 19:14:02 +0100

    feat: Changes in allowed origins, Putting the exact allowed origin

--- backend/src/main/java/org/main/claimstreams/configs/SecurityConfig.java
+++ backend/src/main/java/org/main/claimstreams/configs/SecurityConfig.java
@@ -66,8 +66,7 @@ public class SecurityConfig {
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
                 "http://localhost",
-                "https://claimstreams-frontend-*.vercel.com",
-                "https://claimstreams-*.vercel.com"
+                "https://claimstreams-frontend.vercel.app/"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
